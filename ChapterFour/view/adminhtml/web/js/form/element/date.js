8c32b38416a92b057f115fb222ab7beeed96f180
Tran Khanh Vu <khanhvuu1998@gmail.com>
2019-11-28 10:39:59 +0700

    chapterfour:add showday for ui component

--- ChapterFour/view/adminhtml/web/js/form/element/date.js
+++ ChapterFour/view/adminhtml/web/js/form/element/date.js
@@ -6,25 +6,14 @@ define([
     'use strict';
 
     return Date.extend({
-        initialize: function () {
-            this._super();
-
-
-            var disableddates = ["26-11-2019", "27-11-2019"]; // assign your database dates
-            function DisableSpecificDates(date) {
-                var string = jQuery.datepicker.formatDate('dd-mm-yy', date);
-                return [disableddates.indexOf(string) == -1];
-            }
-
-            $("input[name$='magenest[magenst_date]']").datepicker({
+        defaults: {
+            options: {
                 beforeShowDay: function(d) {
                     var day = d.getDate();
                     return [(day >= 8 && day < 12)];
                 }
-            });
-
-            return this;
-        }
+            },
+        },
     });
 
 });
